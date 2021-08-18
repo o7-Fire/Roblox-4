44ebf17a7bce62bbc81fc9cac1a946659c87c102
Nexity <87981906+Nexity@users.noreply.github.com>
2021-08-18 22:28:55 +0800

    inefficient

--- plane-crazy/c-arrow.lua
+++ plane-crazy/c-arrow.lua
@@ -11,21 +11,21 @@ end)
 Instance.new("Handles", game:GetService("Players")[game.Players.LocalPlayer.Name].PlayerGui)
 game:GetService("Players")[game.Players.LocalPlayer.Name].PlayerGui.Handles.Adornee = 
 ]]
+
 game:GetService("Players")[game.Players.LocalPlayer.Name].PlayerGui.Handles.MouseDrag:Connect(function(Face, Distance)
     local x = os.clock()
-    Distance = math.floor(Distance / 6)
     if Face == Enum.NormalId.Top then
-        move(Vector3.new(0, Distance, 0))
+        move(Vector3.new(0, 1, 0))
     elseif Face == Enum.NormalId.Bottom then
-        move(Vector3.new(0, -Distance, 0))
+        move(Vector3.new(0, -1, 0))
     elseif Face == Enum.NormalId.Left then
-        move(Vector3.new(-Distance, 0, 0))
+        move(Vector3.new(-1, 0, 0))
     elseif Face == Enum.NormalId.Right then
-        move(Vector3.new(Distance, 0, 0))
+        move(Vector3.new(1, 0, 0))
     elseif Face == Enum.NormalId.Front then
-       move(Vector3.new(0, 0, -Distance))
+       move(Vector3.new(0, 0, -1))
     elseif Face == Enum.NormalId.Back then
-        move(Vector3.new(0, 0, Distance))
+        move(Vector3.new(0, 0, 1))
     end
     wait(10 - (os.clock() - x))
 end)
