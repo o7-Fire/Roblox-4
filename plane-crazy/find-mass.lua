d8ba3a75eb1d5d8de47f9259958fcf3228e4546c
Nexity <87981906+Nexity@users.noreply.github.com>
2021-08-22 15:10:38 +0800

    i see

--- plane-crazy/find-mass.lua
+++ plane-crazy/find-mass.lua
@@ -1,3 +1,6 @@
+-- https://docs.google.com/spreadsheets/d/1YJ3EDigS8OPv6il3G1LMLE84fNRiTbUvrPZke6T3o4Y/edit?usp=sharing
+-- you see
+
 for i,v in pairs(game:GetService("Workspace")[game.Players.LocalPlayer.Name]:GetChildren()) do
     pcall(function()
         print(v.Name .. " " .. v.PrimaryPart.Mass)
