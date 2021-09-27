209a96b148355ab7193535fcd89f04e6deb0c431
Nexity <89518595+NexitySecond@users.noreply.github.com>
2021-09-27 20:30:32 +0800

    i dont mind

--- plane-crazy/orbit_stuff.lua
+++ plane-crazy/orbit_stuff.lua
@@ -1,19 +1,39 @@
 local count = 0 --Increment
 _G.Enabled = true
 
+function zeroGrav(part)
+    if part:FindFirstChild("BodyForce") then return end
+    local temp = Instance.new("BodyForce")
+    temp.Force = part:GetMass() * Vector3.new(0,workspace.Gravity,0)
+    temp.Parent = part
+end
+
+local build = game.workspace:WaitForChild(game.Players.LocalPlayer.Name..'Aircraft')
+for i, blocks in pairs(build:GetChildren()) do
+    spawn(function()
+        if blocks.ClassName == "Model" then
+            blocks.PrimaryPart.CanCollide = false
+            zeroGrav(blocks.PrimaryPart)
+        end
+    end)
+end
+    
 while true do
     --Start at origin then apply an offset using Angles()
     if _G.Enabled then
         local build = game.workspace:WaitForChild(game.Players.LocalPlayer.Name..'Aircraft')
         for i, blocks in pairs(build:GetChildren()) do
-            if blocks.ClassName == "Model" then
-                blocks.PrimaryPart.CFrame = CFrame.new(game.Players.LocalPlayer.Character.HumanoidRootPart.Position) * CFrame.Angles(0, count, 0) * CFrame.new(0, 0, 100)
-             
-                --Increment i by 5 degrees every wait()
-                count = count + math.rad(3)
-                wait()
-            end
+            spawn(function()
+                if blocks.ClassName == "Model" and count < 360 then
+                    blocks.PrimaryPart.Velocity = Vector3.new()
+                    blocks.PrimaryPart.CFrame = CFrame.new(game.Players.LocalPlayer.Character.HumanoidRootPart.Position) * CFrame.Angles(0, count, 0) * CFrame.new(0, 0, 50)
+                    count = count + math.rad(3)
+                elseif count > 360 then
+                    count = 0
+                end
+            end)
         end
+        wait(0.1)
     else
         break
     end
