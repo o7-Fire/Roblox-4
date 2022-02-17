51dafb5fa36bf3ad36c3301fb5af3c1f61126764
Nexity <89518595+NexitySecond@users.noreply.github.com>
2022-02-17 16:06:40 +0800

    Update electricstatefarm.lua

--- electricstatefarm.lua
+++ electricstatefarm.lua
@@ -6,6 +6,8 @@ while true do
         break
     end
     wait(1)
+    
+    -- harvest and plant
     for i,v in pairs(game.Workspace.Buildings[plr.Name]:GetChildren()) do
         if v.Name == "Carrot Farm" or v.Name == "Tomato Farm" or v.Name == "Corn Farm" then
             if v["3"].Transparency == 0 or v["1"].Transparency == 1 then
@@ -17,7 +19,7 @@ while true do
             end
         end
     end
-    
+    --sell
     for i,v in pairs(plr.Backpack:GetChildren()) do
         wait(0.5)
         if v.Name == "[Farmer] Carrot" or v.Name == "[Farmer] Tomato" or v.Name == "[Farmer] Corn" then
@@ -30,5 +32,15 @@ while true do
             game:GetService("ReplicatedStorage").Events.MenuActionEvent:FireServer(unpack(args))
         end
     end
+    --redundancy selling
+    for i,v in pairs(plr.Character:GetChildren()) do
+        if v.Name == "[Farmer] Carrot" or v.Name == "[Farmer] Tomato" or v.Name == "[Farmer] Corn" then
+            local args = {
+                [1] = 41,
+                [2] = workspace.Buildings[plr.Name]:FindFirstChild("Capital Cargo Station"),
+                [3] = v
+            }
+            game:GetService("ReplicatedStorage").Events.MenuActionEvent:FireServer(unpack(args))
+        end
+    end
 end
-
