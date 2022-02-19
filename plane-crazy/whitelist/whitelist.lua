e282aaddec31348b4a7ef1660865140a1207f722
Nexity <89518595+NexitySecond@users.noreply.github.com>
2022-02-19 15:07:12 +0800

    fancy gui

--- plane-crazy/whitelist/whitelist.lua
+++ plane-crazy/whitelist/whitelist.lua
@@ -1,4 +1,9 @@
-_G.code = "AVRUERBWI"
+local Flux = loadstring(game:HttpGet"https://raw.githubusercontent.com/dawid-scripts/UI-Libs/main/fluxlib.txt")()
+local Window = Flux:Window("PC Tools", "Press N to open/close", Color3.fromRGB(255, 110, 48), Enum.KeyCode.N)
+local mouse = game.Players.LocalPlayer:GetMouse()
+local uis = game:GetService("UserInputService")
+_G.Whitelist = false -- dont worry about this
+_G.WhitelistCode = ""
 
 function RandomVariable(length)
 	local res = ""
@@ -7,9 +12,74 @@ function RandomVariable(length)
 	end
 	return res
 end
-local DoCheck = RandomVariable(20)
-_G.DoCheck = DoCheck
 
+function Split(s, delimiter)
+    result = {};
+    for match in (s..delimiter):gmatch("(.-)"..delimiter) do
+        table.insert(result, match);
+    end
+    return result;
+end
+
+local DoCheck = RandomVariable(20)
+_G.DoCheck = DoCheck -- for debugging
+
+local Tab1 = Window:Tab("Whitelist", "http://www.roblox.com/asset/?id=6023426915")
+Tab1:Label("Made by Nexity#3200")
+Tab1:Textbox("Whitelist Code", "Create a code and make sure both people have the same code", true, function(text)
+    _G.WhitelistCode = text
+end)
+Tab1:Button("Enable receive data to whitelist user", "Click this to start receiving place remotes from whitelist user", function()
+    _G.Whitelist = true
+end)
+Tab1:Button("Enable send data to whitelist user", "Click this to start sending place remotes to whitelist user", function()
+    _G.Whitelist = false
+end)
+Tab1:Button("destroy this GUI", "does what it says", function()
+    game:GetService("CoreGui").FluxLib:Destroy()
+    _G.DoCheck = ""
+end)
+
+-- request handler because of black magic happening in metatable
+_G.DoDoSend = false
+_G.DoDoToSend = ""
+spawn(function()
+    while true do
+        if _G.DoCheck ~= DoCheck then
+            break
+        end
+        wait()
+        if _G.DoDoSend then
+            _G.DoDoSend = false
+            spawn(function()
+                game:HttpGet(_G.DoDoToSend)
+            end)
+        end
+    end
+end)
+
+-- for receiving
+spawn(function()
+    while true do
+        if _G.DoCheck ~= DoCheck then
+            break
+        end
+        if _G.Whitelist then
+            local r = game:HttpGet("https://WhitelistPC.nexitysecond.repl.co/receive?code=" .. _G.WhitelistCode)
+            local datatodo = Split(r, "SPACE")
+            for i,v in pairs(datatodo) do
+                print(v)
+                spawn(function()
+                    loadstring(v)()
+                end)
+                wait(0.7)
+            end
+        end
+        wait()
+    end
+end)
+
+-- for sending
 local mt = getrawmetatable(game);
 local old = mt.__namecall
 setreadonly(mt,false)
@@ -17,7 +87,7 @@ setreadonly(mt,false)
 mt.__namecall = newcclosure(function(remote,...)
    args = {...}
    method = tostring(getnamecallmethod())
-   if method == "InvokeServer" and tostring(remote) == "PlaceBIockRegion" and _G.DoCheck == DoCheck then
+   if method == "InvokeServer" and tostring(remote) == "PlaceBIockRegion" and _G.DoCheck == DoCheck and _G.Whitelist == false then
         local a2 = tostring(args[2]):gsub(",", "")
         print(args[1])
         local datatosend = table.concat(
@@ -25,7 +95,7 @@ mt.__namecall = newcclosure(function(remote,...)
                 tostring(args[1].x), " ", tostring(args[1].y), " ", tostring(args[1].z) .. "N" .. a2 .. "N" .. tostring(args[3]) .. "N" .. tostring(args[4])
             })
         datatosend = datatosend:gsub(" ", "S")
-        local requesttosend = table.concat({"https://WhitelistPC.nexitysecond.repl.co/send?code=", _G.code, "&data=", datatosend})
+        local requesttosend = table.concat({"https://WhitelistPC.nexitysecond.repl.co/send?code=", _G.WhitelistCode, "&data=", datatosend})
         _G.DoDoSend = true
         _G.DoDoToSend = requesttosend
    end
