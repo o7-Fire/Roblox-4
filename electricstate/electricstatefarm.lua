67eb79cfe12199e980a4370b206fe4a36ae415e8
Nexity <89518595+NexitySecond@users.noreply.github.com>
2022-03-11 00:16:59 +0800

    Create electricstatefarm.lua

---
+++ electricstate/electricstatefarm.lua
@@ -0,0 +1,46 @@
+_G.Enabled = true
+local plr = game.Players.LocalPlayer
+
+while true do
+    if _G.Enabled == false then
+        break
+    end
+    wait(1)
+    
+    -- harvest and plant
+    for i,v in pairs(game.Workspace.Buildings[plr.Name]:GetChildren()) do
+        if v.Name == "Carrot Farm" or v.Name == "Tomato Farm" or v.Name == "Corn Farm" then
+            if v["3"].Transparency == 0 or v["1"].Transparency == 1 then
+                local args = {
+                    [1] = 40,
+                    [2] = v
+                }
+                game:GetService("ReplicatedStorage").Events.MenuActionEvent:FireServer(unpack(args))
+            end
+        end
+    end
+    --sell
+    for i,v in pairs(plr.Backpack:GetChildren()) do
+        wait(0.5)
+        if string.find(v.Name, "Carrot") or string.find(v.Name, "Tomato") or string.find(v.Name, "Corn") then
+            v.Parent = plr.Character
+            local args = {
+                [1] = 41,
+                [2] = workspace.Buildings[plr.Name]:FindFirstChild("Capital Cargo Station"),
+                [3] = plr.Character[v.Name]
+            }
+            game:GetService("ReplicatedStorage").Events.MenuActionEvent:FireServer(unpack(args))
+        end
+    end
+    --redundancy selling
+    for i,v in pairs(plr.Character:GetChildren()) do
+        if string.find(v.Name, "Carrot") or string.find(v.Name, "Tomato") or string.find(v.Name, "Corn") then
+            local args = {
+                [1] = 41,
+                [2] = workspace.Buildings[plr.Name]:FindFirstChild("Capital Cargo Station"),
+                [3] = v
+            }
+            game:GetService("ReplicatedStorage").Events.MenuActionEvent:FireServer(unpack(args))
+        end
+    end
+end
