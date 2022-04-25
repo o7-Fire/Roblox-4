b7bc7bdd978f60c8ba63a91936aa56f6ca3fc3ef
Nexity <89518595+NexitySecond@users.noreply.github.com>
2022-04-25 17:52:08 +0800

    anti anti-crash script

--- syrian-shenanigans/fraggrenadespam.lua
+++ syrian-shenanigans/fraggrenadespam.lua
@@ -1,5 +1,4 @@
 while true do
-    wait(0.2)
     fireclickdetector(game:GetService("Workspace").Map["sandoids spawn"].RefillGivers.Grenade.ClickDetector)
     game.Players.LocalPlayer.Backpack:WaitForChild("Frag Grenade").Parent = game.Workspace[game.Players.LocalPlayer.Name]
 end
