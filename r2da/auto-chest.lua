b3878c2529375669e30272de36b15bbfa66ffd00
Nexity <89518595+NexitySecond@users.noreply.github.com>
2022-05-11 20:16:47 +0800

    Create auto-chest.lua

---
+++ r2da/auto-chest.lua
@@ -0,0 +1,8 @@
+-- teleports chest on top of your head if it exists
+
+while true do
+    wait(1)
+    if game.Workspace.Map:FindFirstChild("RegularChest") then
+        game.Workspace.Map.RegularChest.Chest.Position = game.Players.LocalPlayer.Character.HumanoidRootPart.Position + Vector3.new(0, 2, 0)
+    end
+end
