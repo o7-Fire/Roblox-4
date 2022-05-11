12b2eb6a3fb9d374c1abe13e1a78fba791fd2d83
Nexity <89518595+NexitySecond@users.noreply.github.com>
2022-05-11 20:18:12 +0800

    wait for child

--- r2da/auto-chest.lua
+++ r2da/auto-chest.lua
@@ -2,7 +2,7 @@
 
 while true do
     wait(1)
-    if game.Workspace.Map:FindFirstChild("RegularChest") then
+    if game.Workspace:WaitForChild("Map"):FindFirstChild("RegularChest") then
         game.Workspace.Map.RegularChest.Chest.Position = game.Players.LocalPlayer.Character.HumanoidRootPart.Position + Vector3.new(0, 2, 0)
     end
 end
