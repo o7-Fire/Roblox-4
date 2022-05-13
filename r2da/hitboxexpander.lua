8f0a614c729139945bdfcea29fd685c00a26ea7b
Nexity <89518595+NexitySecond@users.noreply.github.com>
2022-05-13 17:40:09 +0800

    fix

--- r2da/hitboxexpander.lua
+++ r2da/hitboxexpander.lua
@@ -1,14 +1,14 @@
 while true do
     pcall(function()
         for i,v in pairs(game.Workspace.Characters.Survivors:GetChildren()) do
-            if v.Name ~= game.Players.LocalPlayer.Name then
+            if v.Name ~= game.Players.LocalPlayer.Name and v:FindFirstChild("HumanoidRootPart") then
                 v.HumanoidRootPart.Transparency = 1
                 v.HumanoidRootPart.CanCollide = false
                 v.HumanoidRootPart.Size = Vector3.new(10, 10, 10)
             end
         end
         for i,v in pairs(game.Workspace.Characters.Zombies:GetChildren()) do
-            if v.Name ~= game.Players.LocalPlayer.Name then
+            if v.Name ~= game.Players.LocalPlayer.Name and v:FindFirstChild("HumanoidRootPart") then
                 v.HumanoidRootPart.Transparency = 1
                 v.HumanoidRootPart.CanCollide = false
                 v.HumanoidRootPart.Size = Vector3.new(10, 10, 10)
