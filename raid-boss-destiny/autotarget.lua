e30ec589c89349220faf23f0bfd49ba87d20f291
Nexity <89518595+NexitySecond@users.noreply.github.com>
2022-05-27 20:13:53 +0800

    better

--- raid-boss-destiny/autotarget.lua
+++ raid-boss-destiny/autotarget.lua
@@ -1,6 +1,7 @@
 local UserInputService = game:GetService("UserInputService")
 local GuiService = game:GetService("GuiService")
 local LocalPlayer = game:GetService("Players").LocalPlayer
+local Heartbeat = game:GetService("RunService").Heartbeat
 
 function RandomVariable(length)
 	local res = ""
@@ -26,7 +27,7 @@ game:GetService("RunService").Stepped:Connect(function()
                     	if closest == nil then
                     		closest = v.HumanoidRootPart
                     	else
-                    		if (torso.Position.Magnitude - closest.Position.Magnitude) < (torso.Position.Magnitude - v.HumanoidRootPart.Position.Magnitude) then
+                    		if (torso.Position - closest.Position).Magnitude < (torso.Position - v.HumanoidRootPart.Position).Magnitude then
                     			closest = v.HumanoidRootPart
                     		end
                     	end
@@ -42,6 +43,37 @@ game:GetService("RunService").Stepped:Connect(function()
                     game:GetService("ReplicatedStorage").ClientServer.Basic:FireServer(unpack(args))
                 end
                 wait(0.3)
+                doa = true
+            end
+        elseif UserInputService:IsKeyDown(Enum.KeyCode.T) then
+            if doa then
+                doa = false
+                local hrp = game.Players.LocalPlayer.Character.HumanoidRootPart
+                local bosses = game.Workspace.Bosses:GetChildren()
+                closest = nil
+                local torso = game.Players.LocalPlayer.Character.Torso
+                for i, v in pairs(bosses) do
+                    if v:FindFirstChild("HumanoidRootPart") then
+                    	if closest == nil then
+                    		closest = v.HumanoidRootPart
+                    	else
+                    		if (torso.Position - closest.Position).Magnitude < (torso.Position - v.HumanoidRootPart.Position).Magnitude then
+                    			closest = v.HumanoidRootPart
+                    		end
+                    	end
+                	end
+                end
+                hrp.CFrame = CFrame.new(hrp.CFrame.p, closest.Position + (closest.Velocity * 0.5))
+                if closest ~= nil then
+                    local args = {
+                        [1] = closest.Position + (closest.Velocity * 0.5),
+                        [2] = closest,
+                        [3] = Enum.Material.Glass
+                    }
+                    
+                    game:GetService("ReplicatedStorage").ClientServer.Basic:FireServer(unpack(args))
+                end
+                Heartbeat:Wait()
                 doa = true
             end
         end
