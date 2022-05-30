c65f8e278f85f8284160f0a522be3ebc4426e99b
Nexity <89518595+NexitySecond@users.noreply.github.com>
2022-05-30 22:42:51 +0800

    debugging and updated to newer servers

--- minesweeper/safe-and-bomb-finder.lua
+++ minesweeper/safe-and-bomb-finder.lua
@@ -1,4 +1,5 @@
 _G.Enabled = true
+_G.Debugging = false -- change to true if the script doesnt work to see errors
 
 function Split(s, delimiter)
     result = {};
@@ -12,8 +13,8 @@ spawn(function()
     while true do
         wait(5)
         if _G.Enabled then
-            local start = Vector3.new(41.5, 56.5, 71.5)
-            for i,tile in pairs(game.Workspace.MS.Parts:GetChildren()) do
+            local start = Vector3.new(41.5, 56.5, 72.5)
+            for i,tile in pairs(game.Workspace.g.Parts:GetChildren()) do
                 local posx = (tile.Position.X - start.x) / 5
                 local posz = (tile.Position.Z - start.Z) / 5
                 tile.Name = tostring(posx) .. "|" .. tostring(posz)
@@ -27,55 +28,65 @@ end)
 while true do
     wait(0.3)
     if _G.Enabled then
-        if game.Workspace.MS.Parts:FindFirstChild("0|0") then
-            local numbertiles = {}
-            
-            for i,tile in pairs(game.Workspace.MS.Parts:GetChildren()) do
-                if tile.BrickColor == BrickColor.new("Daisy orange") then
-                    table.insert(numbertiles, tile)
-                end
-            end
-            
-            for i,tile in pairs(numbertiles) do
-                local tilepos = Split(tile.Name, "|")
-                local tilex = tonumber(tilepos[1])
-                local tilez = tonumber(tilepos[2])
-                local totalnearbybombs = 0
-                local totalpossiblebombs = 0
-                local safetiles = {}
-                for x=-1, 1, 1 do
-                    for z=-1, 1, 1 do
-                        if game.Workspace.MS.Parts:FindFirstChild(tostring(tilex + x) .. "|" .. tostring(tilez + z)) then
-                            local nearbytile = game.Workspace.MS.Parts[tostring(tilex + x) .. "|" .. tostring(tilez + z)]
-                            if nearbytile.BrickColor == BrickColor.new("Really red") then
-                                totalpossiblebombs = totalpossiblebombs + 1
-                                totalnearbybombs = totalnearbybombs + 1
-                            end
-                            if nearbytile.BrickColor == BrickColor.new("Moss") or nearbytile.BrickColor == BrickColor.new("Shamrock") then
-                                totalpossiblebombs = totalpossiblebombs + 1
-                                table.insert(safetiles, nearbytile)
-                            end
-                        end
+        if game.Workspace.g.Parts:FindFirstChild("0|0") then
+            function thescript()
+                local numbertiles = {}
+                
+                for i,tile in pairs(game.Workspace.g.Parts:GetChildren()) do
+                    if tile.BrickColor == BrickColor.new("Daisy orange") then
+                        table.insert(numbertiles, tile)
                     end
                 end
                 
-                --print(tile.Name, tonumber(tile.SurfaceGui.TextLabel.Text), totalnearbybombs, totalpossiblebombs)
-                if totalnearbybombs == tonumber(tile.SurfaceGui.TextLabel.Text) then
-                    for i,stile in pairs(safetiles) do
-                        if stile.Color ~= Color3.new(0, 1, 0) then
-                            stile.Color = Color3.new(0, 1, 0)
+                for i,tile in pairs(numbertiles) do
+                    local tilepos = Split(tile.Name, "|")
+                    local tilex = tonumber(tilepos[1])
+                    local tilez = tonumber(tilepos[2])
+                    local totalnearbybombs = 0
+                    local totalpossiblebombs = 0
+                    local safetiles = {}
+                    for x=-1, 1, 1 do
+                        for z=-1, 1, 1 do
+                            if game.Workspace.g.Parts:FindFirstChild(tostring(tilex + x) .. "|" .. tostring(tilez + z)) then
+                                local nearbytile = game.Workspace.g.Parts[tostring(tilex + x) .. "|" .. tostring(tilez + z)]
+                                if nearbytile.BrickColor == BrickColor.new("Really red") then
+                                    totalpossiblebombs = totalpossiblebombs + 1
+                                    totalnearbybombs = totalnearbybombs + 1
+                                end
+                                if nearbytile.BrickColor == BrickColor.new("Moss") or nearbytile.BrickColor == BrickColor.new("Shamrock") then
+                                    totalpossiblebombs = totalpossiblebombs + 1
+                                    table.insert(safetiles, nearbytile)
+                                end
+                            end
                         end
                     end
-                elseif totalpossiblebombs == tonumber(tile.SurfaceGui.TextLabel.Text) then
-                    for i,stile in pairs(safetiles) do
-                        if stile:FindFirstChild("TouchInterest") then
-                            stile.TouchInterest:Destroy()
+                    
+                    --print(tile.Name, tonumber(tile.SurfaceGui.TextLabel.Text), totalnearbybombs, totalpossiblebombs)
+                    if totalnearbybombs == tonumber(tile.SurfaceGui.TextLabel.Text) then
+                        for i,stile in pairs(safetiles) do
+                            if stile.Color ~= Color3.new(0, 1, 0) then
+                                stile.Color = Color3.new(0, 1, 0)
+                            end
                         end
-                        if stile.Color ~= Color3.new(1, 0, 0) then
-                            stile.Color = Color3.new(1, 0, 0)
+                    elseif totalpossiblebombs == tonumber(tile.SurfaceGui.TextLabel.Text) then
+                        for i,stile in pairs(safetiles) do
+                            if stile:FindFirstChild("TouchInterest") then
+                                stile.TouchInterest:Destroy()
+                            end
+                            if stile.Color ~= Color3.new(1, 0, 0) then
+                                stile.Color = Color3.new(1, 0, 0)
+                            end
                         end
                     end
                 end
+            end
+            
+            if _G.Debugging then
+                thescript()
+            else
+                pcall(function()
+                    thescript()
+                end)
             end
         end
     else
