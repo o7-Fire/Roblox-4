9cbcc4b0f2677df58d8aad232f8fbb63e52b7198
Nexity <89518595+NexitySecond@users.noreply.github.com>
2022-06-14 17:13:15 +0800

    check if too close

--- r2da/hitboxexpander.lua
+++ r2da/hitboxexpander.lua
@@ -2,9 +2,16 @@ while true do
     pcall(function()
         for i,v in pairs(game.Workspace.Characters.Zombies:GetChildren()) do
             if v.Name ~= game.Players.LocalPlayer.Name and v:FindFirstChild("HumanoidRootPart") and not v:FindFirstChild("NoThrow") then
-                v.HumanoidRootPart.Transparency = 1
-                v.HumanoidRootPart.CanCollide = false
-                v.HumanoidRootPart.Size = Vector3.new(10, 10, 10)
+                magnitude = (game.Players.LocalPlayer.Character.HumanoidRootPart.Position - v.HumanoidRootPart.Position).magnitude
+                if magnitude > 15 then
+                    v.HumanoidRootPart.Transparency = 1
+                    v.HumanoidRootPart.CanCollide = false
+                    v.HumanoidRootPart.Size = Vector3.new(10, 10, 10)
+                else
+                    v.HumanoidRootPart.Transparency = 1
+                    v.HumanoidRootPart.CanCollide = false
+                    v.HumanoidRootPart.Size = Vector3.new(3, 3, 3)
+                end
             end
         end
     end)
