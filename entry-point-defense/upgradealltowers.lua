0dfb6579a09ee07715306507d45f00a6210c824c
Nexity <89518595+NexitySecond@users.noreply.github.com>
2022-07-01 18:49:51 +0800

    Create upgradealltowers.lua

---
+++ entry-point-defense/upgradealltowers.lua
@@ -0,0 +1,32 @@
+local dowait = true
+local theremote = game:GetService("ReplicatedStorage").UpgradeEvent
+
+function upgrade(v)
+    for i=1, 6, 1 do
+        local args = {
+            [1] = game:GetService("Players").LocalPlayer,
+            [2] = v
+        }
+        theremote:FireServer(unpack(args))
+        if dowait then
+            wait(.2)
+        end
+    end
+    local args = {
+        [1] = game:GetService("Players").LocalPlayer,
+        [2] = v,
+        [3] = "A"
+    }
+    theremote:FireServer(unpack(args))
+end
+
+for i,v in pairs(game.Workspace.Towers:GetChildren()) do
+    if v.Owner.Value == game.Players.LocalPlayer.Name then
+        if v.Upgrades:FindFirstChild("6") then
+            upgrade(v)
+            if dowait then
+                wait(1)
+            end
+        end
+    end
+end
