1bcb4c7b432c04641652ca7abfdd5baf1b18e55d
Nexity <89518595+NexitySecond@users.noreply.github.com>
2022-07-01 18:25:30 +0800

    Update buffedfarm.lua

--- entry-point-defense/buffedfarm.lua
+++ entry-point-defense/buffedfarm.lua
@@ -1,10 +1,21 @@
--- gives the overclock effect to your server farms lol
+local foryouonly = true
+
+function activate(v)
+    local args = {
+        [1] = v,
+        [2] = v.Abilities.AbilityID
+    }
+    game:GetService("ReplicatedStorage").AbilityEvent:FireServer(unpack(args))
+end
+
 for i,v in pairs(game.Workspace.Towers:GetChildren()) do
-    if v.Name == "Server Farm" then
-        local args = {
-            [1] = v,
-            [2] = v.Abilities.AbilityID
-        }
-        game:GetService("ReplicatedStorage").AbilityEvent:FireServer(unpack(args))
+    if foryouonly then
+        if v.Name == "Server Farm" and v.Owner.Value == game.Players.LocalPlayer.Name then
+            activate(v)
+        end
+    else
+        if v.Name == "Server Farm" then
+            activate(v)
+        end
     end
 end
