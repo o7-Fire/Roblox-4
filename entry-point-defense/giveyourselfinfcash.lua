11e920a4067a14ef9552342b80f224c90e36e8ec
Nexity <89518595+NexitySecond@users.noreply.github.com>
2022-07-02 19:46:05 +0800

    Update giveyourselfinfcash.lua

--- entry-point-defense/giveyourselfinfcash.lua
+++ entry-point-defense/giveyourselfinfcash.lua
@@ -1,9 +1,25 @@
---lol
-local randomPlayer = game.Players:GetPlayers()[math.random(1,#game.Players:GetPlayers())]
+--lol, btw it resets a random person's cash so you can have 100q, you greedy bastard
+local giveeveryonecash = false
 
-local args = {
-    [1] = randomplayer,
-    [2] = -99999999999999999
-}
+function givemoney(plr, amount)
+    local args = {
+        [1] = plr,
+        [2] = amount
+    }
+    game:GetService("ReplicatedStorage").GiveEvent:FireServer(unpack(args))
+end
 
-game:GetService("ReplicatedStorage").GiveEvent:FireServer(unpack(args))
+local randomPlayer = game.Players:GetChildren()[math.random(1,#game.Players:GetChildren())]
+givemoney(randomPlayer, 99999999999999999)
+givemoney(randomPlayer, -99999999999999999)
+
+if giveeveryonecash then
+    for i,v in pairs(game.Players:GetChildren()) do
+        local args = {
+            [1] = v,
+            [2] = math.random(10000000, 500000000)
+        }
+        
+        game:GetService("ReplicatedStorage").GiveEvent:FireServer(unpack(args))
+    end
+end
