b9fcd60763b418eecae8803b4923eba2f55b7dce
Itzbenz <49940811+Itzbenz@users.noreply.github.com>
2022-07-23 10:42:48 +0000

    Create spam_bomb_controlled.lua

---
+++ naval-warfare/spam_bomb_controlled.lua
@@ -0,0 +1,11 @@
+local UserInputService = game:GetService("UserInputService")
+local GuiService = game:GetService("GuiService")
+local LocalPlayer = game:GetService("Players").LocalPlayer
+
+local doa = true
+
+game:GetService("RunService").Stepped:Connect(function()
+    if UserInputService:IsKeyDown(Enum.KeyCode.B) then
+        game:GetService("ReplicatedStorage").Event:FireServer("bomb")
+    end
+end)
