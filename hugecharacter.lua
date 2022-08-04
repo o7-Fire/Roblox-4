fe0e3f0f90d3627080e80e70454ae6b56454a28a
Nexity <89518595+NexitySecond@users.noreply.github.com>
2022-08-04 15:00:53 +0800

    Update hugecharacter.lua

--- hugecharacter.lua
+++ hugecharacter.lua
@@ -7,6 +7,7 @@ local doa = true
 game:GetService("RunService").Stepped:Connect(function()
     if UserInputService:IsKeyDown(Enum.KeyCode.B) then
         if doa then
+            doa = false
             fireclickdetector(workspace.ClothingRacks.ClothingRack.Hitbox.ClickDetector)
             for _,v in ipairs(game:GetService("Players").LocalPlayer.Character:GetDescendants()) do
                 if v.Name == "OriginalSize" then v:Destroy() end
@@ -14,7 +15,8 @@ game:GetService("RunService").Stepped:Connect(function()
             for _,v in ipairs(game:GetService("Players").LocalPlayer.Character.Humanoid:GetChildren()) do
                 if string.find(v.Name,"Scale") then v:Destroy() end
             end
+            doa = true
         end
         wait(.1)
     end
-end
+end)
