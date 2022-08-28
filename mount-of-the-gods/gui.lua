46c40362539acb7eed29dad1cea42d318ed654a5
Nexity <89518595+NexitySecond@users.noreply.github.com>
2022-08-28 16:46:35 +0800

    Update gui.lua

--- mount-of-the-gods/gui.lua
+++ mount-of-the-gods/gui.lua
@@ -1,9 +1,13 @@
 local Flux = loadstring(game:HttpGet"https://raw.githubusercontent.com/dawid-scripts/UI-Libs/main/fluxlib.txt")()
 local Window = Flux:Window("MOTG", "Press N to open/close", Color3.fromRGB(255, 110, 48), Enum.KeyCode.N)
+local character = game.Players.LocalPlayer.Character
 
 local RS = game:GetService("ReplicatedStorage")
-local drinkremote = RS.Resources.Remotes.DrinkFromWell
-local interactremote = RS.Resources.Remotes.InteractItem
+local Remotes = RS.Resources.Remotes
+local drinkremote = Remotes.DrinkFromWell
+local interactremote = Remotes.InteractItem
+local endseasonremote = Remotes.EndSeason
+local makefishremote = Remotes.MakeFish
 
 function drink()
     for i,v in pairs(game.Workspace.Buildings:GetChildren()) do
@@ -23,8 +27,11 @@ function use(v, x)
 end
 
 local Tab1 = Window:Tab("Misc", "http://www.roblox.com/asset/?id=6023426915")
-Tab1:Button('Drink', "drinks from wells", function()
+Tab1:Button("Drink", "drinks from wells", function()
     drink()
+end)
+Tab1:Button("End Season", "ends current season", function()
+    endseasonremote:FireServer()
 end)
 Tab1:Button('Destroy this GUI', "boom", function()
     game:GetService("CoreGui").FluxLib:Destroy()
@@ -33,13 +40,21 @@ end)
 local touse = ""
 local amounttouse = 0
 local Tab2 = Window:Tab("Use", "http://www.roblox.com/asset/?id=6023426915")
-Tab2:Textbox('Object to Use', 'Name Here', true, function(output)
+Tab2:Textbox("Object To Use", "Name Here", true, function(output)
     touse = tostring(output)
 end)
-Tab2:Textbox('Amount of Times', 'Amount Here', true, function(output)
+Tab2:Textbox("Amount of Times", "Amount Here", true, function(output)
     amounttouse = tonumber(output)
 end)
-Tab2:Button('Use', "uses the item inf amount of times without it being destroyed", function()
-    use(game.Players.LocalPlayer.Character[touse], amounttouse)
+Tab2:Button("Use", "uses the item inf amount of times without it being destroyed", function()
+    use(character[touse], amounttouse)
 end)
 
+local fishtosummon = ""
+local Tab3 = Window:Tab("Fish", "http://www.roblox.com/asset/?id=6023426915")
+Tab3:Textbox("Fish To Spawn", "Name Here", true, function(output)
+    fishtosummon = tostring(output)
+end)
+Tab3:Button("Spawn", "spawns the fish, requires you to have fishing rod out", function()
+    makefishremote:FireServer(fishtosummon, character.HumanoidRootPart.Position)
+end)
