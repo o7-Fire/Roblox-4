abb70767e6dcd4b73999a707112ae5eeda93ba6d
Nexity <89518595+NexitySecond@users.noreply.github.com>
2022-11-09 19:26:08 +0800

    Create main.lua

---
+++ rate-my-avatar/screenshare/main.lua
@@ -0,0 +1,24 @@
+_G.Enabled = true
+if not _G.Enabled then
+    d()
+end
+
+local updater = game:GetService("ReplicatedStorage").CustomiseBooth
+function update(text)
+    local args = {
+        [1] = "Update",
+        [2] = {
+            ["DescriptionText"] = text,
+            ["ImageId"] = 0
+        }
+    }
+    
+    updater:FireServer(unpack(args))
+end
+
+while _G.Enabled do
+    local r = game:HttpGet("http://192.168.0.166:8080")
+    print(r)
+    coroutine.wrap(update)(r)
+    task.wait(.03)
+end
