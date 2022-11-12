bedaafe63eb86419c9b0c87a0a45e4f09d8a8fc2
Nexity <89518595+NexitySecond@users.noreply.github.com>
2022-11-12 13:24:20 +0800

    Create image_render.py

---
+++ rate-my-avatar/screenshare/image_render.py
@@ -0,0 +1,27 @@
+from PIL import Image
+from io import BytesIO
+import base64
+import requests
+
+emojidata = requests.get('https://unicode.org/emoji/charts/full-emoji-list.html').text
+emojistodo = "💚🥬💧💡🍏🍓"
+
+def to_base64_png(emoji, version=0):
+    html_search_string = r"<img alt='{}' class='imga' src='data:image/png;base64,([^']+)'>" #'
+    matchlist = re.findall(html_search_string.format(emoji), emojidata)
+    #print(matchlist)
+    return matchlist[version]
+
+finalstring = ""
+for emoji in emojistodo:
+    try:
+        b64 = to_base64_png(emoji)
+        image = Image.open( BytesIO(base64.b64decode(b64)) )
+        #reduced = image.convert("P", palette=Image.WEB)
+        image = image.convert("RGBA")
+        image = image.resize((1,1), resample=0)
+        c = image.getpixel((0,0))
+        finalstring += f'({str(c[0])}, {str(c[1])}, {str(c[2])}, "{emoji}"),\n'
+    except Exception as e:
+        print("error: " + str(e))
+print(finalstring)
